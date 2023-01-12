cf682f3503beadd478cb1ad0f0c59a22109a9609
Amadeusz Majcher <amadeusz.majcher@shoper.pl>
2023-01-12 09:57:45 +0100

    js zad1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-const postLoaderButton = document.querySelector(".post--loader");
-const hiddenPost = document.querySelector(".post.hidden");
+// Zadanie 1
+const tab = [1,2,3,4];
+function checkTable(table,x){
+    return table.includes(x);
+};
 
-postLoaderButton.addEventListener("click", function() {
-    hiddenPost.classList.remove("hidden");
-});
+console.log(tab);
+console.log(checkTable(tab,11));
+// Zadanie 2
+const tablestring = ["ala ma kota", "test test", "xxx"];
+
