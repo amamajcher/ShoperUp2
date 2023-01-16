1fd38581cc7c541ab0a35f1fa4b60bb1278bcbea
Amadeusz Majcher <amadeusz.majcher@shoper.pl>
2023-01-16 18:22:17 +0100

    zad2

--- js/app.js
+++ js/app.js
@@ -5,7 +5,17 @@ function checkTable(table,x){
 };
 
 console.log(tab);
-console.log(checkTable(tab,11));
+console.log(checkTable(tab,2));
+
 // Zadanie 2
 const tablestring = ["ala ma kota", "test test", "xxx"];
 
+function convertStringToArrays(tablestring, separator){
+    let emptytable = [];
+    for (let i=0; i<tablestring.length; i++){
+        emptytable.push(tablestring[i].split(separator));
+    }
+
+    return emptytable;
+}
+console.log(convertStringToArrays(tablestring, " "));
